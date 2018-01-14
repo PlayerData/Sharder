2d894083d7ef7526c4ee93d37d0d028f6f328490
Hayden Ball <hayden@haydenball.me.uk>
2018-01-14 20:00:20 +0000

    Correct migrator spec Migration definitions

--- spec/integration/migrator_spec.rb
+++ spec/integration/migrator_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe Sharder::Migrator do
   it "raises a helpful error message if no shard_group is defined" do
-    class Migration < ActiveRecord::Migration::Current
+    class ShardNotSetMigration < ActiveRecord::Migration::Current
       def change; end
 
       def self.version
@@ -12,15 +12,15 @@ RSpec.describe Sharder::Migrator do
       end
     end
 
-    expect { ActiveRecord::Migrator.new(:up, [Migration]).migrate }.to(
+    expect { ActiveRecord::Migrator.new(:up, [ShardNotSetMigration]).migrate }.to(
       raise_error Sharder::Migrator::NoShardGroupSpecifiedError
     )
 
-    expect(ActiveRecord::SchemaMigration.where(version: Migration.version)).to_not exist
+    expect(ActiveRecord::SchemaMigration.where(version: ShardNotSetMigration.version)).to_not exist
   end
 
   it "does not error when no database names are returned by the configurator for a migration" do
-    class Migration < ActiveRecord::Migration::Current
+    class UnknownShardMigration < ActiveRecord::Migration::Current
       self.shard_group = :unknown_group
 
       def change
@@ -32,15 +32,15 @@ RSpec.describe Sharder::Migrator do
       end
     end
 
-    ActiveRecord::Migrator.new(:up, [Migration]).migrate
-    expect(ActiveRecord::SchemaMigration.where(version: Migration.version)).to exist
+    ActiveRecord::Migrator.new(:up, [UnknownShardMigration]).migrate
+    expect(ActiveRecord::SchemaMigration.where(version: UnknownShardMigration.version)).to exist
 
-    ActiveRecord::Migrator.new(:down, [Migration]).migrate
-    expect(ActiveRecord::SchemaMigration.where(version: Migration.version)).to_not exist
+    ActiveRecord::Migrator.new(:down, [UnknownShardMigration]).migrate
+    expect(ActiveRecord::SchemaMigration.where(version: UnknownShardMigration.version)).to_not exist
   end
 
   it "runs and rolls back a migration on for the default database" do
-    class Migration < ActiveRecord::Migration::Current
+    class ValidMigration < ActiveRecord::Migration::Current
       self.shard_group = :default
 
       def change
@@ -52,13 +52,13 @@ RSpec.describe Sharder::Migrator do
       end
     end
 
-    ActiveRecord::Migrator.new(:up, [Migration]).migrate
-    expect(ActiveRecord::SchemaMigration.where(version: Migration.version)).to exist
+    ActiveRecord::Migrator.new(:up, [ValidMigration]).migrate
+    expect(ActiveRecord::SchemaMigration.where(version: ValidMigration.version)).to exist
     ClubIndex.reset_column_information
     ClubIndex.create!(name: "Migration Test", tests: 2)
 
-    ActiveRecord::Migrator.new(:down, [Migration]).migrate
-    expect(ActiveRecord::SchemaMigration.where(version: Migration.version)).to_not exist
+    ActiveRecord::Migrator.new(:down, [ValidMigration]).migrate
+    expect(ActiveRecord::SchemaMigration.where(version: ValidMigration.version)).to_not exist
     ClubIndex.reset_column_information
     expect { ClubIndex.create!(name: "Migration Test", tests: 2) }.to(
       raise_error ActiveModel::UnknownAttributeError
--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -5,5 +5,7 @@ RSpec.configure do |config|
     ClubIndex.find_each do |club_index|
       club_index.database.destroy if club_index.database.exists?
     end
+
+    ClubIndex.destroy_all
   end
 end
