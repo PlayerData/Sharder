079abf2a8adc09b1392d4ce5d79604f368838784
Hayden Ball <hayden@haydenball.me.uk>
2018-04-15 17:56:46 +0100

    Use a separate instance of Rails' migrator for each shard

--- lib/sharder/migrator.rb
+++ lib/sharder/migrator.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# We replace ActiveRecord::Migrator with Sharder::Migrator in sharder/extensions
+# This gives us access to Rails' original migrator
+RailsMigrator = ActiveRecord::Migrator.clone
+
 class Sharder
   module Migrator
     class NoShardGroupSpecifiedError < StandardError
@@ -15,33 +19,39 @@ class Sharder
       end
     end
 
+    attr_reader :default_shard_migrator
+
+    delegate :current_version, :current_migration, :current, :pending_migrations, to: :default_shard_migrator
+
+    def initialize(direction, migrations, target_version = nil)
+      @direction = direction
+      @migrations = migrations
+      @target_version = target_version
+
+      @default_shard_migrator = RailsMigrator.new(direction, migrations, target_version)
+
+      validate(@migrations)
+    end
+
+    def run
+      on_each_shard do |shard_group|
+        shard_migrations = @migrations.select { |m| m.shard_group == shard_group }
+        RailsMigrator.new(@direction, shard_migrations, @target_version).run
+      end
+
+      record_default_version_state_after_migrating
+    end
+
+    def migrate
+      on_each_shard do |shard_group|
+        shard_migrations = @migrations.select { |m| m.shard_group == shard_group }
+        RailsMigrator.new(@direction, shard_migrations, @target_version).migrate
+      end
+
+      record_default_version_state_after_migrating
+    end
+
     private
-
-    def execute_migration_in_transaction(migration, direction)
-      shard_group = migration.shard_group
-      raise NoShardGroupSpecifiedError unless shard_group
-
-      database_names = configurator.database_names_for_shard_group(shard_group)
-      database_names ||= []
-
-      database_names.each do |database_name|
-        Sharder.using(database_name) { super }
-      end
-
-      return if database_names.include?(:default)
-
-      record_version_state_after_migrating(migration.version, include_cache: true)
-    end
-
-    def record_version_state_after_migrating(version, include_cache: false)
-      if down?
-        migrated.delete(version) if include_cache
-        ActiveRecord::SchemaMigration.where(version: version.to_s).delete_all
-      else
-        migrated << version if include_cache
-        ActiveRecord::SchemaMigration.create!(version: version.to_s)
-      end
-    end
 
     def sharder_connection
       ActiveRecord::Base.establish_connection.connection
@@ -49,6 +59,32 @@ class Sharder
 
     def configurator
       sharder_connection.configurator
+    end
+
+    def validate(migrations)
+      raise NoShardGroupSpecifiedError if migrations.select { |m| m.shard_group.nil? }.any?
+    end
+
+    def on_each_shard
+      configurator.shard_groups.map do |shard_group|
+        shards = configurator.database_names_for_shard_group(shard_group)
+        shards.each do |shard|
+          Sharder.using(shard) do
+            yield shard_group
+          end
+        end
+      end
+    end
+
+    def record_default_version_state_after_migrating
+      @migrations.each do |migration|
+        if @direction == :down
+          ActiveRecord::SchemaMigration.where(version: migration.version.to_s).delete_all
+        else
+          next if migration.shard_group == :default
+          ActiveRecord::SchemaMigration.create!(version: migration.version.to_s)
+        end
+      end
     end
   end
 end
--- spec/integration/migrator_spec.rb
+++ spec/integration/migrator_spec.rb
@@ -3,6 +3,14 @@
 require "rails_helper"
 
 RSpec.describe Sharder::Migrator do
+  let(:club_index) { ClubIndex.create!(name: "Test") }
+  let(:club_index2) { ClubIndex.create!(name: "Test 2") }
+
+  before do
+    club_index.database.create
+    club_index2.database.create
+  end
+
   it "raises a helpful error message if no shard_group is defined" do
     class ShardNotSetMigration < ActiveRecord::Migration::Current
       def change; end
@@ -39,8 +47,8 @@ RSpec.describe Sharder::Migrator do
     expect(ActiveRecord::SchemaMigration.where(version: UnknownShardMigration.version)).to_not exist
   end
 
-  it "runs and rolls back a migration on for the default database" do
-    class ValidMigration < ActiveRecord::Migration::Current
+  it "runs and rolls back a migration for the default database" do
+    class DefaultShardMigration < ActiveRecord::Migration::Current
       self.shard_group = :default
 
       def change
@@ -52,21 +60,22 @@ RSpec.describe Sharder::Migrator do
       end
     end
 
-    ActiveRecord::Migrator.new(:up, [ValidMigration]).migrate
-    expect(ActiveRecord::SchemaMigration.where(version: ValidMigration.version)).to exist
-    ClubIndex.reset_column_information
-    ClubIndex.create!(name: "Migration Test", tests: 2)
+    ActiveRecord::Migrator.new(:up, [DefaultShardMigration]).migrate
+    expect(ActiveRecord::SchemaMigration.where(version: DefaultShardMigration.version)).to exist
 
-    ActiveRecord::Migrator.new(:down, [ValidMigration]).migrate
-    expect(ActiveRecord::SchemaMigration.where(version: ValidMigration.version)).to_not exist
+    ClubIndex.reset_column_information
+    ClubIndex.create!(name: "Migration Test", tests: 2).destroy!
+
+    ActiveRecord::Migrator.new(:down, [DefaultShardMigration]).migrate
+    expect(ActiveRecord::SchemaMigration.where(version: DefaultShardMigration.version)).to_not exist
     ClubIndex.reset_column_information
     expect { ClubIndex.create!(name: "Migration Test", tests: 2) }.to(
       raise_error ActiveModel::UnknownAttributeError
     )
   end
 
-  it "migrates sharded databases" do
-    class ValidMigration < ActiveRecord::Migration::Current
+  describe "migrating sharded databases" do
+    class ShardMigration < ActiveRecord::Migration::Current
       self.shard_group = :clubs
 
       def change
@@ -78,34 +87,50 @@ RSpec.describe Sharder::Migrator do
       end
     end
 
-    club_index = ClubIndex.create!(name: "Test")
-    club_index.database.create
+    it "runs a migration in both directions on each shard" do
+      ActiveRecord::Migrator.new(:up, [ShardMigration]).migrate
+      expect(ActiveRecord::SchemaMigration.where(version: ShardMigration.version)).to exist
 
-    club_index2 = ClubIndex.create!(name: "Test 2")
-    club_index2.database.create
+      [club_index, club_index2].each do |index|
+        index.database.switch do
+          expect(ActiveRecord::SchemaMigration.where(version: ShardMigration.version)).to exist
 
-    ActiveRecord::Migrator.new(:up, [ValidMigration]).migrate
-    expect(ActiveRecord::SchemaMigration.where(version: ValidMigration.version)).to exist
+          Staff.reset_column_information
+          Staff.create!(name: "Migration Test", tests: 2)
+        end
+      end
 
-    [club_index, club_index2].each do |index|
-      index.database.switch do
-        expect(ActiveRecord::SchemaMigration.where(version: ValidMigration.version)).to exist
+      ActiveRecord::Migrator.new(:down, [ShardMigration]).migrate
+      expect(ActiveRecord::SchemaMigration.where(version: ShardMigration.version)).to_not exist
 
-        Staff.reset_column_information
-        Staff.create!(name: "Migration Test", tests: 2)
+      [club_index, club_index2].each do |index|
+        index.database.switch do
+          expect(ActiveRecord::SchemaMigration.where(version: ShardMigration.version)).to_not exist
+          Staff.reset_column_information
+          expect { Staff.create!(name: "Migration Test", tests: 2) }.to(
+            raise_error ActiveModel::UnknownAttributeError
+          )
+        end
       end
     end
 
-    ActiveRecord::Migrator.new(:down, [ValidMigration]).migrate
-    expect(ActiveRecord::SchemaMigration.where(version: ValidMigration.version)).to_not exist
+    it "runs migrations on unmigrated shards following an error in migration" do
+      club_index.database.switch do
+        # Simulate club_index already having migrated
+        ShardMigration.migrate(:up)
+        ActiveRecord::SchemaMigration.create!(version: ShardMigration.version)
+      end
 
-    [club_index, club_index2].each do |index|
-      index.database.switch do
-        expect(ActiveRecord::SchemaMigration.where(version: ValidMigration.version)).to_not exist
-        Staff.reset_column_information
-        expect { Staff.create!(name: "Migration Test", tests: 2) }.to(
-          raise_error ActiveModel::UnknownAttributeError
-        )
+      ActiveRecord::Migrator.new(:up, [ShardMigration]).migrate
+      expect(ActiveRecord::SchemaMigration.where(version: ShardMigration.version)).to exist
+
+      [club_index, club_index2].each do |index|
+        index.database.switch do
+          expect(ActiveRecord::SchemaMigration.where(version: ShardMigration.version)).to exist
+
+          Staff.reset_column_information
+          Staff.create!(name: "Migration Test", tests: 2)
+        end
       end
     end
   end
